3be1a9037761ec0cc6605e0cd42c194459d9a6b3
ACBob <ethbot@hotmail.com>
2019-07-09 23:48:47 +0100

    Change .JS, Now attempts to modify Paragraphs and all types of headers.

--- gaben.js
+++ gaben.js
@@ -3,6 +3,10 @@ javascript: function GabeN(ratio, imageurl) {
   this.imageurl = imageurl
 }
 
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+} //Give arrays a "Random Element" option.
+
 var getGabeN = {
   init: function(myGabeN) {
     this.myGabeN = myGabeN
@@ -44,6 +48,25 @@ new GabeN("square", "http://i.imgur.com/ecbdpfL.jpg"),
 new GabeN("square", "http://i.imgur.com/lDkMf5P.jpg"), 
 new GabeN("square", "http://i.imgur.com/qkcP48v.jpg")];
 
+var GabenTexts = [
+"GabeN",
+"gAbEn",
+"Gabe Newell",
+"All you need is Gaben.",
+"Gabe newell.",
+"Gabeity Gabe.",
+"Gabe Gabe Gabe",
+"Gabe is love, Gabe is life.",
+"A little bit of GabeN in my life",
+"A little bit of GabeN by my side",
+"A little bit of GabeN is all I need",
+"A little bit of GabeN is what I see",
+"A little bit of GabeN in the sun",
+"(GabeN #5)",
+"Gaben is our god, truly.",
+"GAABENN"
+]
+
 function imageRatio(image) {
   var proportion = image.height / image.width;
   if (proportion > 1) return "vertical";
@@ -51,10 +74,14 @@ function imageRatio(image) {
   else if (proportion < 1) return "horizontal"
   }(function(document) {
     getGabeN.init(myGabeN);
-    var images = document.getElementsByTagName("img"),
-    length = images.length;
-    for (var i = 0; i < length; i++) {
+    var images = document.getElementsByTagName("img");
+    var paragraphs = document.querySelectorAll("p,h1,h2,h3,h4,h5,h6");
+    imgslength = images.length;
+    pslength = paragraphs.length;
+    console.log(pslength);
+    for (var i = 0; i < imgslength; i++) {
       var ratio = imageRatio(images[i]);
+      //console.log("Image Change");
       if (ratio === "horizontal") {
         var number = Randomize(getGabeN.horizontal());
         var img = getGabeN.horizontal()[number];
@@ -68,5 +95,11 @@ function imageRatio(image) {
         var img = getGabeN.vertical()[number];
         images[i].src = img.imageurl
       }
+    }
+    for (var i = 0; i < pslength; i++)
+    {
+        console.log(paragraphs[i]);
+        paragraphs[i].innerHTML = GabenTexts.randomElement();
+        //console.log("Paragraph Change");
     }
   })(document);
